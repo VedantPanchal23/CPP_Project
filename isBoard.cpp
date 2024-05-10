2f5710c3ef6d60005382c0182072dad2f477022e
VedantPanchal23 <148608437+VedantPanchal23@users.noreply.github.com>
2024-05-10 16:45:05 +0530

    23AIML042

---
+++ isBoard.cpp
@@ -0,0 +1,14 @@
+bool isBoardFull(const vector<vector<char>> &board)
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board[i][j] == ' ')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
